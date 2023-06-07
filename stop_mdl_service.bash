a4b6d6e2690ad33b438442c11784a624dcfffb49
chenlong-cl <70930517+chenlong-cl@users.noreply.github.com>
2023-06-07 09:42:40 +0800

    Delete stop_mdl_service.bash

--- stop_mdl_service.bash
+++
@@ -1,5 +0,0 @@
-sudo systemctl stop adas_detected.service
-sudo systemctl stop adas_laser_pub.service
-sudo systemctl stop adas_pub.service
-sudo systemctl stop dsm_detected.service
-sudo systemctl stop dsm_pub.service
